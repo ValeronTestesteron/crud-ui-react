da19d0e3d1af2e0793a5dec50a6208ee31d63bb9
konstantin pyatkov <kopyatkov@yandex.ru>
2021-06-09 05:31:09 +0300

    Updates

---
+++ precache-manifest.8c49e8202e654b3a09298b43f0533cef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f91b28fa0c2587f6357769c703258ba",
+    "url": "/crud-ui-react/index.html"
+  },
+  {
+    "revision": "c1fe9769f67ae6fb9260",
+    "url": "/crud-ui-react/static/css/main.986107d2.chunk.css"
+  },
+  {
+    "revision": "c346de844e3ae17aa99c",
+    "url": "/crud-ui-react/static/js/2.5d84b070.chunk.js"
+  },
+  {
+    "revision": "c1fe9769f67ae6fb9260",
+    "url": "/crud-ui-react/static/js/main.6c895483.chunk.js"
+  },
+  {
+    "revision": "a9440ec97190ca227ab1",
+    "url": "/crud-ui-react/static/js/runtime-main.9391924a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/crud-ui-react/static/media/logo.ee7cd8ed.svg"
+  }
+]);
