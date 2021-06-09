5b721916c43cc79ac921ea19e7ef7391369cab99
konstantin pyatkov <kopyatkov@yandex.ru>
2021-06-09 04:13:26 +0300

    build

---
+++ build/precache-manifest.cc9443077f1814f4030938505dcb00d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b5c0451757745a02751f1646600ffc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9b086d6fd20b82994bb",
+    "url": "/static/css/main.986107d2.chunk.css"
+  },
+  {
+    "revision": "c346de844e3ae17aa99c",
+    "url": "/static/js/2.5d84b070.chunk.js"
+  },
+  {
+    "revision": "e9b086d6fd20b82994bb",
+    "url": "/static/js/main.f4f32444.chunk.js"
+  },
+  {
+    "revision": "254dcbfc20c791900808",
+    "url": "/static/js/runtime-main.f57b0557.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
